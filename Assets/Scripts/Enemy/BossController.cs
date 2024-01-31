7eac35247321075d66ad4059e498f490d1c31e91
Roichew <roi_chew01@yahoo.com>
2024-02-01 01:37:47 +0800

    boss colour change

--- Assets/Scripts/Enemy/BossController.cs
+++ Assets/Scripts/Enemy/BossController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    public GameObject Boss;
+    public bool inMotion;
+
+    [Header("Colour")]
+    public Material redMat;
+    public Material blueMat;
+    public Material greenMat;
+    public bool isChanged;
+    public bool isRed;
+    public bool isBlue;
+    public bool isGreen;
+    public float colourTimer;
+
     private enum bossState
     {
         Spawn,
@@ -18,7 +31,51 @@ public class BossController : MonoBehaviour
         PhaseFour
 
     }
+    private void Awake()
+    {  
+
+    }
+
+    private void Update()
+    {
+        if (!isChanged && !inMotion)
+        {
+            Invoke("colorChange", colourTimer);
+            isChanged = true;
+        }
+    }
 
 
+    public void colorChange()
+    {
+        print("change!");
+        int randomNumber = Random.Range(1, 4);
+        switch (randomNumber)
+        {
+            //Red
+            case 1:
+                Boss.tag = "Red";
+                Boss.GetComponent<Renderer>().material = redMat;
+                print(Boss.gameObject.tag);
+                isChanged = false;
+                break;
+            //Green   
+            case 2:
+                Boss.tag = "Green";
+                Boss.GetComponent<Renderer>().material = greenMat;
+                print(Boss.gameObject.tag);
+                isChanged = false;
+                break;
+            //Blue
+            case 3:
+
+                Boss.tag = "Blue";
+                Boss.GetComponent<Renderer>().material = blueMat;
+                print(Boss.gameObject.tag);
+                isChanged = false;
+                break;
+        }
+        
+    }
 }
 
