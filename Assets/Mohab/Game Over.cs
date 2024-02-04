20ca9627a2faa4ada22591f88e3df28603d280e2
mohabq <mhab46019@gmail.com>
2024-02-05 06:09:29 +0800

    Basic Win and Game Over Screen

---
+++ Assets/Mohab/Game Over.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject LoseScreen; // Assign your Win Screen GameObject in the inspector
+    void Start()
+    {
+        // Ensure the win screen is not visible when the game starts
+        LoseScreen.SetActive(false);
+    }
+    void Update()
+    {
+        // When the 'L' key is pressed, display the win screen
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            LoseScreen.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Mohab/Game Victory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameVictory : MonoBehaviour
+{
+    public GameObject winScreen; // Assign your Win Screen UI GameObject in the inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the win screen is not visible when the game starts
+        winScreen.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // When the 'L' key is pressed, display the win screen
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            winScreen.SetActive(true);
+        }
+    }
+}
